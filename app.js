6c8255ebe5d1b6e00558e1d3e6b0af1fdbf4dd3e
nbirhanu <nbirhanu80@gmail.com>
2024-01-24 22:50:18 +0300

    make GET api link

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 //using express
 const app = express();
 
-app.get('/', (req, res) => {
+//read file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'hello from the server side!!!',
-    data: 'natours',
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
