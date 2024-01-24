9e3a2346ebbe083bc01a97f92e0044928cf44b3d
nbirhanu <nbirhanu80@gmail.com>
2024-01-24 23:54:23 +0300

    add document to tours api

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 //using express
 const app = express();
 
+app.use(express.json());
+
 //read file
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+//GET API
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -17,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+//POST DOCUMENT API
+
+app.post('/api/v1/tours', (req, res) => {
+  const newTour = Object.assign(req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
