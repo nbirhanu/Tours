1da65c00f1db56301032b08d17783bd79dfb100a
nbirhanu <nbirhanu80@gmail.com>
2024-01-26 14:38:33 +0300

    build user handler

---
+++ controller/userController.js
@@ -0,0 +1,31 @@
+//usersfunctions
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'athis route is not yet defined!!!',
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'athis route is not yet defined!!!',
+  });
+};
+exports.getUserById = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'athis route is not yet defined!!!',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'athis route is not yet defined!!!',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'athis route is not yet defined!!!',
+  });
+};
