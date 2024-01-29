e7290b736fb3bb52a7ffa51f61954e59f8f09a7c
nbirhanu <nbirhanu80@gmail.com>
2024-01-29 23:26:00 +0300

    import database string

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
+const DBString = process.env.DBSTRING.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
