6e9d5a14583377c71191e947ac427b982dbecc15
nbirhanu <nbirhanu80@gmail.com>
2024-01-30 17:07:58 +0300

    connect database string with project with mongoose

--- server.js
+++ server.js
@@ -2,12 +2,39 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 const DBString = process.env.DBSTRING.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
+
+mongoose.connect(DBString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Connection events
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+// Close the Mongoose connection when the Node process is terminated
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
