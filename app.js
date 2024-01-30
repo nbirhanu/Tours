dc535f740ce81c37b2f7d84aa3907e9e1723cd8f
nbirhanu <nbirhanu80@gmail.com>
2024-01-30 22:50:07 +0300

    set environment

--- app.js
+++ app.js
@@ -1,151 +1,21 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
-//using express
+const tourRouter = require('./Router/tourRouter');
+const userRouter = require('./Router/userRouter');
+
+// using express
 const app = express();
 
-//MIDDLEWARE
+// MIDDLEWARE
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
 
-//read file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-//GET ALL API
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-//POST API
-const createTour = (req, res) => {
-  const newTour = Object.assign(req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
-};
-
-// GET DOCUMENT BY ID
-const getTourById = (req, res) => {
-  const id = +req.params.id;
-
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-//UPDATE TOUR
-const updateTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Update tour here....>',
-    },
-  });
-};
-//DELETE TOUR
-const deleteTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: {
-      tour: null,
-    },
-  });
-};
-
-//usersfunctions
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'athis route is not yet defined!!!',
-  });
-};
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'athis route is not yet defined!!!',
-  });
-};
-const getUserById = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'athis route is not yet defined!!!',
-  });
-};
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'athis route is not yet defined!!!',
-  });
-};
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'athis route is not yet defined!!!',
-  });
-};
-
-//ROUTES
-const userRouter = express.Router();
-const tourRouter = express.Router();
-//middleware
+// ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-//tourRouter
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
-//userRouter
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
-});
+module.exports = app;
