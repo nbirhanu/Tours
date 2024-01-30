897491eb9cea3d74e9c9a6656eac5cf94501c2cc
nbirhanu <nbirhanu80@gmail.com>
2024-01-30 21:16:54 +0300

    find tour by id

--- controller/userController.js
+++ controller/userController.js
@@ -1,31 +1,31 @@
-//usersfunctions
+// usersfunctions
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'athis route is not yet defined!!!',
-  });
-};
+  })
+}
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'athis route is not yet defined!!!',
-  });
-};
+  })
+}
 exports.getUserById = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'athis route is not yet defined!!!',
-  });
-};
+  })
+}
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'athis route is not yet defined!!!',
-  });
-};
+  })
+}
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'athis route is not yet defined!!!',
-  });
-};
+  })
+}
