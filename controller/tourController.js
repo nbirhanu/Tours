17d5f6b731be0dc6dd5ddf91a310b34eb181b614
nbirhanu <nbirhanu80@gmail.com>
2024-02-02 23:36:11 +0300

    field limiting

--- controller/tourController.js
+++ controller/tourController.js
@@ -41,7 +41,12 @@ exports.getAllTours = async (req, res) => {
     }
 
     //FIELDS
-
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     //EXECUTE
     const tours = await query;
 
