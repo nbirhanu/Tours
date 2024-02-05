9b6c058834ddc045c51b1254417c203f37675718
nbirhanu <nbirhanu80@gmail.com>
2024-02-05 22:38:02 +0300

    build url for monthly plan document

--- Router/tourRouter.js
+++ Router/tourRouter.js
@@ -7,6 +7,9 @@ const router = express.Router();
 //tour-stats
 router.route('/tour-stats').get(tourController.tourStats);
 
+//monthly-plane
+router.route('/monthly-plan/:year').get(tourController.monthlyPlane);
+
 router
   .route('/')
   .get(tourController.getAllTours)
